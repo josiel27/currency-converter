78234613bf72f5fe8bd52a64616cffd8bd11f994
Josiel Faria <josiel@farmaservice.com.br>
2019-10-04 12:01:05 -0300

    Updates

---
+++ precache-manifest.68e288afa61b82a5312fbd8ec627bb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5db5da4b740781d663d99b141dcd526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77731a20d3ba389d4163",
+    "url": "/static/css/main.f49f3503.chunk.css"
+  },
+  {
+    "revision": "5ab4742599540a223387",
+    "url": "/static/js/2.a90208ca.chunk.js"
+  },
+  {
+    "revision": "77731a20d3ba389d4163",
+    "url": "/static/js/main.466d85cc.chunk.js"
+  },
+  {
+    "revision": "c1aaf1b2963ad611dc70",
+    "url": "/static/js/runtime-main.f593f705.js"
+  }
+]);
